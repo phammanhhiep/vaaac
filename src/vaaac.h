254b675d4f53aaa5034555c2ce7bf66f9ce40daa
pablo peñarroja <49104978+soybin@users.noreply.github.com>
2020-06-14 03:17:41 +0200

    upd

--- src/vaaac.h
+++ src/vaaac.h
@@ -3,33 +3,33 @@
  * Copyright (c) 2020 Pablo Peñarroja
  */
 
-/*
- * the following constants should be
- * altered in order to fit the user's
- * needs.
- * they're set to standard values
- * by default, and changing them may
- * dramatically alter the library's
- * behaviour
- */
+ /*
+  * the following constants should be
+  * altered in order to fit the user's
+  * needs.
+  * they're set to standard values
+  * by default, and changing them may
+  * dramatically alter the library's
+  * behaviour
+  */
 
-//                                   //
-//-------- c o n s t a n t s --------//
-//                                   //
+  //                                   //
+  //-------- c o n s t a n t s --------//
+  //                                   //
 
-/*
- * this indicates whether to render
- * the processed image to a matrix 
- * 'frame'.
- * this matrix can be retrieved using
- * the 'getFrame()' method declared
- * bellow
- */
+  /*
+   * this indicates whether to render
+   * the processed image to a matrix
+   * 'frame'.
+   * this matrix can be retrieved using
+   * the 'getFrame()' method declared
+   * bellow
+   */
 const bool RENDER_TO_FRAME = true;
 
 /*
  * this indicates whether to create a
- * separate window and render the 
+ * separate window and render the
  * current frame to that window.
  * it requires the 'RENDER_TO_FRAME'
  * constant to be true
@@ -47,18 +47,19 @@ const bool RENDER_SAMPLE_TEXT = true;
  * width and height of the skin color
  * sampling rectangle
  */
-const int SAMPLE_AREA_WIDTH = 40;
-const int SAMPLE_AREA_HEIGHT = 40;
+const int SAMPLE_AREA_WIDTH = 30;
+const int SAMPLE_AREA_HEIGHT = 30;
 
 /*
- * the bfs sample size represents the 
- * square root of the area to scan for
- * each node of the bfs algorithm.
- * the lower this value, the more 
- * accurate but the more noise, and the
- * more cpu usage required as well
+ * this is the tolerance applied to
+ * the lower and higher bounds of the
+ * HSV skin tone color sampled from
+ * the user.
+ * low values limit the range, thus
+ *
  */
-const int BFS_SAMPLE_SIZE = 4;
+const int MASK_LOW_TOLERANCE = 50;
+const int MASK_HIGH_TOLERANCE = 25;
 
 /*
  * width and height of the reticle:
@@ -71,33 +72,36 @@ const int BFS_SAMPLE_SIZE = 4;
 const int RETICLE_SIZE = 40;
 
 /*
- * this is the tolerance applied to 
- * the lower and higher bounds of the
- * HSV skin tone color sampled from
- * the user.
- * low values limit the range, thus
- * 
+ * if an object is detected, but the
+ * aim point is located within this
+ * area, the aim point will be instantly
+ * set to zero.
+ * the reason for this being that it's
+ * very hard to distinguish the accurate
+ * location of the aim point when the
+ * user is pointing straight at the
+ * camera
  */
-const int MASK_LOW_TOLERANCE = 50;
-const int MASK_HIGH_TOLERANCE = 25;
+const int NO_AIM_AREA_SIZE = 80;
 
 /*
- * the variance of the y component of
- * the angle for the last k frames is
- * stored in the 'yDelta' double-ended
- * queue.
- * this deque is processed every frame
- * trying to find a peak shape in it.
- * if a peak is found, then, it's
- * understood that an action has been
- * triggered.
- * as a consequence of this, the 
- * boolean value 'triggered' is set 
- * true for that frame.
- * this value has to be greater than
- * two
+ * the bfs sample size represents the
+ * square root of the area to scan for
+ * each node of the bfs algorithm.
+ * the lower this value, the more
+ * accurate but the more noise, and the
+ * more cpu usage required as well
  */
-const int TRIGGER_QUEUE_SIZE = 15;
+const int BFS_SAMPLE_SIZE = 2;
+
+/*
+ * smoothness is used to get rid of some
+ * of the noise in the calculations.
+ * the higher this number, the more
+ * smoothness, less noise, but less
+ * responsiveness as well
+ */
+const int AIM_SMOOTHNESS = 4;
 
 /*
  * this is the minumum distance that
@@ -107,7 +111,17 @@ const int TRIGGER_QUEUE_SIZE = 15;
  * it's represented as a percentage
  * of the screen real estate
  */
-const double TRIGGER_MINIMUM_DISTANCE = 3.5;
+const double TRIGGER_MINIMUM_DISTANCE = 3.0;
+
+/*
+ * this is the maximum distance that
+ * the aim point is allowed to  travel 
+ * upwards in order to be considered a
+ * trigger action.
+ * it's represented as a percentage
+ * of the screen real estate
+ */
+const double TRIGGER_MAXIMUM_DISTANCE = 15.0;
 
 /*
  * consider y1 as the origin y value
@@ -120,13 +134,13 @@ const double TRIGGER_MINIMUM_DISTANCE = 3.5;
  * it's represented as a percentage of
  * the screen real estate
  */
-const double TRIGGER_ALLOWED_Y_DEVIATION = 6.0;
+const double TRIGGER_ALLOWED_Y_DEVIATION = 3.0;
 
 /*
  * same as 'TRIGGER_ALLOWED_Y_DEVIATION'
  * but for the x-axis
  */
-const double TRIGGER_ALLOWED_X_DEVIATION = 3.0;
+const double TRIGGER_ALLOWED_X_DEVIATION = 2.0;
 
 //                                 //
 //--------  v  a  a  a  c  --------//
@@ -144,70 +158,84 @@ const double TRIGGER_ALLOWED_X_DEVIATION = 3.0;
 #include <opencv2/opencv.hpp>
 
 class vaaac {
-	private:
-		// is everything working
-		int ok;
+private:
+	// is everything working
+	int ok;
 
-		// resolution (square)
-		int width;
-		int height;
-		int res;
-		int halfRes;
+	// player detected
+	bool detected;
 
-		// computer vision vars
-		cv::VideoCapture videoCapture;
-		cv::Rect frameBounds;
-		cv::Rect reticleBounds;
-		cv::Mat frame;
-		cv::Mat mask;
+	// resolution (square)
+	int width;
+	int height;
+	int res;
+	int halfRes;
 
-		// skin tone hsv color bounds
-		int hLow;
-		int hHigh;
-		int sLow;
-		int sHigh;
-		int vLow;
-		int vHigh;
+	// computer vision vars
+	cv::VideoCapture videoCapture;
+	cv::Rect frameBounds;
+	cv::Rect reticleBounds;
+	cv::Rect noAimAreaBounds;
+	cv::Mat frame;
+	cv::Mat mask;
 
-		// breadth first search system
-		std::vector<std::pair<int, int>> bfsOffsets;
-		
-		// aim point location
-		double xAngle;
-		double yAngle;
+	// skin tone hsv color bounds
+	int hLow;
+	int hHigh;
+	int sLow;
+	int sHigh;
+	int vLow;
+	int vHigh;
 
-		// trigger system
-		bool triggered;
-		int TRIGGER_HALF_QUEUE_SIZE;
-		int TRIGGER_MINIMUM_DISTANCE_PIXELS;
-		int TRIGGER_ALLOWED_Y_DEVIATION_PIXELS;
-		int TRIGGER_ALLOWED_X_DEVIATION_PIXELS;
-		std::deque<std::pair<int, int>> yDelta;
+	// breadth first search system
+	std::vector<std::pair<int, int>> bfsOffsets;
 
-	public:
-		vaaac();
-		~vaaac();
+	// aimed at point location
+	double xAngle;
+	double yAngle;
 
-		inline bool isOk() {
-			return ok == 2;
-		}
+	// stepped point location
+	double xAngleSmooth;
+	double yAngleSmooth;
 
-		inline bool isTriggered() {
-			return triggered;
-		}
+	// trigger system
+	bool triggered;
+	bool increment;
+	int yxDeltaSize;
+	int TRIGGER_MINIMUM_DISTANCE_PIXELS;
+	int TRIGGER_MAXIMUM_DISTANCE_PIXELS;
+	int TRIGGER_ALLOWED_Y_DEVIATION_PIXELS;
+	int TRIGGER_ALLOWED_X_DEVIATION_PIXELS;
+	std::vector<std::pair<int, int>> yxDelta;
 
-		inline double getXAngle() {
-			return xAngle;
-		}
+public:
+	vaaac();
+	~vaaac();
 
-		inline double getYAngle() {
-			return yAngle;
-		}
+	inline bool isOk() {
+		return ok == 2;
+	}
 
-		inline cv::Mat getFrame() {
-			return frame;
-		}
+	inline bool isDetected() {
+		return detected;
+	}
 
-		void calibrateSkinTone();
-		void update();
+	inline bool isTriggered() {
+		return triggered;
+	}
+
+	inline double getXAngle() {
+		return xAngleSmooth;
+	}
+
+	inline double getYAngle() {
+		return yAngleSmooth;
+	}
+
+	inline cv::Mat getFrame() {
+		return frame;
+	}
+
+	void calibrateSkinTone();
+	void update();
 };
