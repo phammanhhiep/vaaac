fdb0023a7be3689409359520766a37fd4e092e1e
kuwoko <owavemail@gmail.com>
2020-06-15 17:04:19 +0200

    queue del size

--- vaaac.hpp
+++ vaaac.hpp
@@ -3,28 +3,28 @@
  * Copyright (c) 2020 Pablo Peñarroja
  */
 
- /*
-  * the following constants should be
-  * altered in order to fit the user's
-  * needs.
-  * they're set to standard values
-  * by default, and changing them may
-  * dramatically alter the library's
-  * behaviour
-  */
+/*
+ * the following constants should be
+ * altered in order to fit the user's
+ * needs.
+ * they're set to standard values
+ * by default, and changing them may
+ * dramatically alter the library's
+ * behaviour
+ */
 
-  //                                   //
-  //-------- c o n s t a n t s --------//
-  //                                   //
+ //                                   //
+ //-------- c o n s t a n t s --------//
+ //                                   //
 
-  /*
-   * this indicates whether to render
-   * the processed image to a matrix
-   * 'frame'.
-   * this matrix can be retrieved using
-   * the 'getFrame()' method declared
-   * bellow
-   */
+/*
+ * this indicates whether to render
+ * the processed image to a matrix
+ * 'frame'.
+ * this matrix can be retrieved using
+ * the 'getFrame()' method declared
+ * bellow
+ */
 const bool RENDER_TO_FRAME = true;
 
 /*
